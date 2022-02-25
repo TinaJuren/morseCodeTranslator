eb638dbd90878259becade7163beb0ed7f0b28a8
tinaj <tina.juren@gmail.com>
2022-02-25 16:43:10 +0100

    Updated HashMap.

--- src/TestTranslator.java
+++ src/TestTranslator.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class TestTranslator {
 
@@ -22,6 +23,36 @@ public class TestTranslator {
         String actual = translator.getEnglishLetter("-");
 
         assertEquals("T", actual);
+    }
+
+    @Test
+    public void testEToAsterix() {
+
+        Translator translator = new Translator();
+
+        String actual = translator.getMorseCode("E");
+
+        assertEquals("*", actual);
+    }
+
+    @Test
+    public void testUnvalidMorseCode() {
+
+        Translator translator = new Translator();
+
+        String actual = translator.getMorseCode("*****");
+
+        assertNull(actual);
+    }
+
+    @Test
+    public void testUnvalidEnglishLetter() {
+
+        Translator translator = new Translator();
+
+        String actual = translator.getMorseCode("Ö");
+
+        assertNull(actual);
     }
 
 
--- src/Translator.java
+++ src/Translator.java
@@ -3,17 +3,77 @@ import java.util.HashMap;
 public class Translator {
 
     private final HashMap<String, String> englishLetter;
+    private final HashMap<String, String> morseCode;
 
     public Translator() {
+
         englishLetter = new HashMap<>();
 
+        englishLetter.put("*-", "A");
+        englishLetter.put("_***", "B");
+        englishLetter.put("-*-*", "C");
+        englishLetter.put("-**", "D");
         englishLetter.put("*", "E");
+        englishLetter.put("**-*", "F");
+        englishLetter.put("--*", "G");
+        englishLetter.put("****", "H");
+        englishLetter.put("**", "I");
+        englishLetter.put("*---", "J");
+        englishLetter.put("-*-", "K");
+        englishLetter.put("*-**", "L");
+        englishLetter.put("--", "M");
+        englishLetter.put("-*", "N");
+        englishLetter.put("---", "O");
+        englishLetter.put("*--*", "P");
+        englishLetter.put("--*-", "Q");
+        englishLetter.put("*-*", "R");
+        englishLetter.put("***", "S");
         englishLetter.put("-", "T");
+        englishLetter.put("**-", "U");
+        englishLetter.put("***-", "V");
+        englishLetter.put("*--", "W");
+        englishLetter.put("-**-", "X");
+        englishLetter.put("-*--", "Y");
+        englishLetter.put("--**", "Z");
+        englishLetter.put("/", " ");
+
+        morseCode = new HashMap<>();
+
+        morseCode.put("A", "*-");
+        morseCode.put("B", "_***");
+        morseCode.put("C", "-*-*");
+        morseCode.put("D", "-**");
+        morseCode.put("E", "*");
+        morseCode.put("F", "**-*");
+        morseCode.put("G", "--*");
+        morseCode.put("H", "****");
+        morseCode.put("I", "**");
+        morseCode.put("J", "*---");
+        morseCode.put("K", "-*-");
+        morseCode.put("L", "*-**");
+        morseCode.put("M", "--");
+        morseCode.put("N", "-*");
+        morseCode.put("O", "---");
+        morseCode.put("P", "*--*");
+        morseCode.put("Q", "--*-");
+        morseCode.put("R", "*-*");
+        morseCode.put("S", "***");
+        morseCode.put("T", "-");
+        morseCode.put("U", "**-");
+        morseCode.put("V", "***-");
+        morseCode.put("W", "*--");
+        morseCode.put("X", "-**-");
+        morseCode.put("Y", "-*--");
+        morseCode.put("Z", "--**");
+        morseCode.put(" ", " ");
     }
 
-    public String getEnglishLetter (String s){
-            return englishLetter.get(s);
+    public String getEnglishLetter (String s) {
+        return englishLetter.get(s);
     }
 
+    public String getMorseCode(String e) {
+        return morseCode.get(e);
+    }
 
 }
